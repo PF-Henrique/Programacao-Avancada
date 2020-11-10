86ff9f1c58f44c6475234bfd67443981ded56f25
Pedro Henrique Ferreira <phenrique@opportunity.oppti>
2020-11-09 21:59:13 -0300

    memento complete code first

---
+++ Estudo/01 - Programação avançada/Padrão de projetos/Template Method/templateMethodExemple3/Futebol.java
@@ -0,0 +1,14 @@
+public class Futebol extends Game {
+
+   void endPlay() {
+      System.out.println("O jogo de Futebol foi finalizado!");
+   }
+
+   void initialize() {
+      System.out.println("inicializando o jogo de Futebol! Começe a jogar.");
+   }
+
+   void startPlay() {
+      System.out.println("Jogo de Vôlei iniciado. Aproveite o jogo!");
+   }
+}
